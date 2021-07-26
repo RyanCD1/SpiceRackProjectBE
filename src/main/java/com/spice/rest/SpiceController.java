c4bf1b719a402ae4909b10326c93a79d02c1acef
RyanCD1 <ryandonaghue0@gmail.com>
2021-07-26 17:34:35 +0100

    added Spice class and controller

---
+++ src/main/java/com/spice/rest/SpiceController.java
@@ -0,0 +1,50 @@
+package com.spice.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spice.data.Spice;
+
+@RestController
+public class SpiceController {
+
+	private List<Spice> spices = new ArrayList<>();
+
+	@PostMapping("/createSpice")
+	public void createKitten(@RequestBody Spice spice) {
+		System.out.println(spice);
+		this.spices.add(spice);
+	}
+
+	@GetMapping("/getAllSpices")
+	public List<Spice> getAllSpices() {
+		return this.spices;
+	}
+
+	@GetMapping("/getSpice/{id}")
+	public Spice getSpice(@PathVariable int id) {
+		Spice found = this.spices.get(id);
+		return found;
+	}
+
+	@PutMapping("/replaceSpice/{id}")
+	public Spice replaceSpice(@PathVariable int id, @RequestBody Spice newSpice) {
+		return this.spices.set(id, newSpice);
+	}
+
+	@DeleteMapping("/deleteSpice/{id}")
+	public String deleteSpice(@PathVariable int id) {
+		this.spices.remove(id);
+
+		return "Deleted Spice at index: " + id;
+	}
+
+}
