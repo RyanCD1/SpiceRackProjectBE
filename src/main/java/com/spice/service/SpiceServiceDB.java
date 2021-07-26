7004d6239b72d0337beb6f1a60e1691c9437aa6f
RyanCD1 <ryandonaghue0@gmail.com>
2021-07-26 17:50:24 +0100

    added service db with pre-generaated methods

---
+++ src/main/java/com/spice/service/SpiceServiceDB.java
@@ -0,0 +1,42 @@
+package com.spice.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spice.data.Spice;
+
+@Service
+public class SpiceServiceDB implements SpiceService {
+
+	@Override
+	public void createSpice(Spice spice) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Spice> getAllSpices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Spice getSpice(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Spice replaceSpice(int id, Spice newSpice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteSpice(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
